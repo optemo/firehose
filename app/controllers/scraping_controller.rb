ae5851a5b4c26a17b6756be342d9e991bb307e6c
Jan Ulrich <jan@optemo.com>
2010-12-15 12:45:25 -0800

    Changed to Touchscreen cameras

--- app/controllers/scraping_controller.rb
+++ app/controllers/scraping_controller.rb
@@ -4,7 +4,7 @@ class ScrapingController < ApplicationController
     if params[:category_id]
       @category_id = params[:category_id]
     else
-      @category_id = 20218 # This is hard-coded to be digital cameras from Best Buy's feed
+      @category_id = 29171#20218 # This is hard-coded to be digital cameras from Best Buy's feed
     end
     @product_skus = BestBuyApi.category_ids(@category_id)
   end
@@ -12,7 +12,7 @@ class ScrapingController < ApplicationController
   def scrape
     @id = params[:id]
     @raw_info = BestBuyApi.product_search(@id)
-    @product_for_display = PP.pp(@raw_info, "")
+    @product_for_display = PP.pretty_print(@raw_info, "")
     @scraped_features = {}
     unless @raw_info.nil?
       rules = ScrapingRule.find_all_by_product_type(Session.product_type)
