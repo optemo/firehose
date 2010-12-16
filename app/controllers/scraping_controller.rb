f12479f5eb83a031988078747c62ea3a6471a51c
Zev Thompson <zev@optemo.com>
2010-12-16 13:19:35 -0800

    Rule adding is now possible

--- app/controllers/scraping_controller.rb
+++ app/controllers/scraping_controller.rb
@@ -34,4 +34,15 @@ class ScrapingController < ApplicationController
     @scraping_rule = ScrapingRule.new
   end
 
+  def create
+    # Creates a new scraping rule.
+    # There are three kinds of rules, continuous, binary, and categorical.
+    @scraping_rule = ScrapingRule.new(params[:rule])
+    @scraping_rule.product_type = Session.product_type
+    if @scraping_rule.valid?
+      @scraping_rule.save
+    else
+      # error
+    end
+  end
 end
