0cd948be948a84c8ba66dfd137d263c5f62409fc
Jan Ulrich <jan@optemo.com>
2011-02-02 16:52:50 -0800

    Fixed the calculate factors function to only update, not renew factors

--- app/models/product.rb
+++ app/models/product.rb
@@ -143,32 +143,30 @@ class Product < ActiveRecord::Base
     cont_activerecords = []
     #cat_activerecords =[]
     #bin_activerecords = []
-    cont_spec_local_cache = {} # This saves doing many ContSpec lookups. It's a hash with {id => value} pairs
+    records = {}
+    record_vals = {}
+    factors = {}
     all_products = Product.valid.instock
     all_products.each do |product|
       utility = []
       s.continuous["filter"].each do |f|
-        unless cont_spec_local_cache[f]
-          records = ContSpec.find(:all, :select => 'product_id, value', :conditions => ["product_id IN (?) and name = ?", all_products, f])
-          temp_hash = {}
-          records.each do |r| # Strip the records down to {id => value} pairs
-            temp_hash[r.product_id] = r.value
-          end
-          cont_spec_local_cache[f] = temp_hash
-        end
-        newFactorRow = ContSpec.new({:product_id => product.id, :product_type => s.product_type, :name => f+"_factor"})
-        fVal = cont_spec_local_cache[f][product.id]
-        debugger unless fVal # The alternative here is to crash. This should never happen if Product.valid.instock is doing its job.
-        newFactorRow.value = Product.calculateFactor(fVal, f, cont_spec_local_cache[f])
-        utility << newFactorRow.value
-        cont_activerecords.push(newFactorRow)
+        records[f] ||= ContSpec.where(["product_id IN (?) and name = ?", all_products, f]).group_by(&:product_id)
+        record_vals[f] ||= records[f].values.map{|i|i.first.value}
+        factors[f] ||= ContSpec.where(["product_id IN (?) and name = ?", all_products, f+"_factor"]).group_by(&:product_id)
+        factorRow = factors[f][product.id].first || ContSpec.new(:product_id => product.id, :product_type => s.product_type, :name => f+"_factor")
+        fVal = records[f][product.id].first
+        debugger unless fVal && fVal.value # The alternative here is to crash. This should never happen if Product.valid.instock is doing its job.
+        factorRow.value = Product.calculateFactor(fVal.value, f, record_vals[f])
+        utility << factorRow.value
+        cont_activerecords << factorRow
       end
       #Add the static calculated utility
-      cont_activerecords.push ContSpec.new({:product_id => product.id, :product_type => s.product_type, :name => "utility", :value => utility.sum})
+      utilities ||= ContSpec.where(["product_id IN (?) and name = ?", all_products, "utility"]).group_by(&:product_id)
+      product_utility = utilities[product.id].first || ContSpec.new({:product_id => product.id, :product_type => s.product_type, :name => "utility"})
+      product_utility.value = utility.sum
+      cont_activerecords << product_utility
     end
 
-    ContSpec.delete_all(["name = 'utility' and product_type = ?", s.product_type])
-    s.continuous["filter"].each{|f| ContSpec.delete_all(["name = ? and product_type = ?", f+"_factor", s.product_type])} # ContSpec records do not have a version number, so we have to wipe out the old ones.  
     # Do all record saving at the end for efficiency
     ContSpec.transaction do
       cont_activerecords.each(&:save)
@@ -182,9 +180,11 @@ class Product < ActiveRecord::Base
     end
   end
   
+  private
+  
   def self.calculateFactor(fVal, f, contspecs)
     # Order the feature values, reversed to give the highest value to duplicates
-    ordered = contspecs.values.sort
+    ordered = contspecs.sort
     ordered = ordered.reverse if Session.current.prefDirection[f] == 1
     return 0 if Session.current.prefDirection[f] == 0
     pos = ordered.index(fVal)
