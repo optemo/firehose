d26d026de84ec9db5c90c61ee7c320fe3b88f663
Jan Ulrich <jan@optemo.com>
2011-05-05 16:48:05 -0700

    BB iframe work

--- public/javascripts/loader.js
+++ public/javascripts/loader.js
@@ -1,10 +1,15 @@
 function r(f){!!document.body?f():setTimeout('r('+f+')',9)}
 r(function(){
 var ef = document.createElement("iframe");
-ef.src = "http://bbembed.optemo.com?embedding=true&iframe=true";
+//Check for History
+var history = location.hash.replace(/^#/, '');
+if (history.length > 0)
+	ef.src = "http://192.168.5.100:3000?onestage=true&hist="+history;
+else
+	ef.src = "http://192.168.5.100:3000?onestage=true";
 ef.id = "optemo_iframe";
 ef.width = "754px";
-ef.height = "1070px";
+ef.height = "1170px";
 ef.setAttribute("style", "border:none;position:absolute;left:76.6px;top:60px;");
 //document.getElementById("optemo_embedder").appendChild(ef);
 //document.body.appendChild(ef);
