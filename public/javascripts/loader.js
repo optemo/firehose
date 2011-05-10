6a9cdfca07bb4f761592b68b5f98a7fcf6d77113
Jan Ulrich <jan@optemo.com>
2011-05-10 13:33:25 -0700

    Ready for speed testing

--- public/javascripts/loader.js
+++ public/javascripts/loader.js
@@ -1,22 +1,21 @@
-function r(f){!!document.body?f():setTimeout('r('+f+')',9)}
-r(function(){
+function opt_r(f){!!document.body?f():setTimeout('opt_r('+f+')',9)}
+opt_r(function(){
 var ef = document.createElement("iframe");
 //Check for History
 var history = location.hash.replace(/^#/, '');
+var REMOTE = "http://192.168.5.100:3000";
 if (history.length > 0)
-	ef.src = "http://192.168.5.100:3000?onestage=true&hist="+history;
+	ef.src = REMOTE+"?onestage=true&hist="+history;
 else
-	ef.src = "http://192.168.5.100:3000?onestage=true";
+	ef.src = REMOTE+"?onestage=true";
 ef.id = "optemo_iframe";
 ef.width = "754px";
 ef.height = "1170px";
-ef.setAttribute("style", "border:none;position:absolute;left:76.6px;top:60px;");
-//document.getElementById("optemo_embedder").appendChild(ef);
-//document.body.appendChild(ef);
+ef.setAttribute("frameborder","0");
 document.body.insertBefore(ef,document.body.firstChild);
 });
-function rr(f){/in/.test(document.readyState)?setTimeout('rr('+f+')',9):f()}
-rr(function(){
+function opt_s(f){/in/.test(document.readyState)?setTimeout('opt_s('+f+')',9):f()}
+opt_s(function(){
 	var pos = $('#optemo_embedder').offset();
 	$('#optemo_iframe').css({'left': pos.left, 'top': pos.top}).show();
 });
