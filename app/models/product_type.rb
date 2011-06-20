6334aedd74ac7f7cb81138f65d107fce9b36a1a9
Ray <ray@rayimac.(none)>
2011-06-20 16:15:49 -0700

    Fix the bug of associations of product_types

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,7 +1,7 @@
 class ProductType < ActiveRecord::Base
-  has_many :product_type_urls
-  has_many :product_type_headings
-  has_many :product_type_features, :through => :product_type_headings
+  has_many :urls
+  has_many :headings
+  has_many :features, :through => :headings
 
   validates_presence_of :name
   validates_presence_of :category_id
