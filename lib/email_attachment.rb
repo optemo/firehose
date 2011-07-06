3f4cb0ba4ec20356a5930a5245f43bed06fec623
Jan Ulrich <jan@optemo.com>
2011-07-06 16:40:20 -0700

    Works with real omniture email now

--- lib/email_attachment.rb
+++ lib/email_attachment.rb
@@ -8,6 +8,7 @@ def product_orders
   msgs = imap.search(["SINCE", "1-Jan-1969"]) 
   # Read each message 
   msgs.reverse.each do |msgID| 
+    debugger
     msg = imap.fetch(msgID, ["ENVELOPE","UID","BODY"] )[0] 
   # Only those with 'SOMETEXT' in subject are of our interest 
     if msg.attr["ENVELOPE"].from[0].host == "omniture.com"
@@ -17,8 +18,8 @@ def product_orders
   # additional attachments attributes 
         i+=1 
         next if body.parts[i-1].param.nil? || body.parts[i-1].media_type.nil?
-        cType = body.parts[i-1].media_type 
-        cName = "#{Rails.root}/tmp/"+body.parts[i-1].param['NAME'] 
+        next unless body.parts[i-1].media_type == "APPLICATION"
+        cName = "#{Rails.root}/tmp/#{Time.now.strftime("%y-%m-%d")}.zip" 
         
   # fetch attachment. 
         attachment = imap.fetch(msgID, "BODY[#{i}]")[0].attr["BODY[#{i}]"] 
@@ -28,24 +29,28 @@ def product_orders
         end
   # Unzip file
         #I coulnd't figure out how to unzip a string, otherwise we could do this whole thing in memory instead of temp files
+        csvfile = ""
         Zip::ZipFile.open(cName) do |zip_file|
            zip_file.each do |f|
              f_path=File.join("#{Rails.root}/tmp/", f.name)
+             csvfile = f_path
              FileUtils.mkdir_p(File.dirname(f_path))
              zip_file.extract(f, f_path) unless File.exist?(f_path)
            end
          end
   # Open csv file
         contspecs = []
-        File.open(cName.gsub('.zip','.csv'), 'r') do |f|
-          f.each do |line|
-            /\d+\.,,(?<sku>[^,]+),,(?<rev>"?\$\d+(,\d+)?"?),,,,[^,]+,,(?<orders>\d+)/ =~ line
-            if sku
-              product = Product.find_by_sku(sku)
-              if product
-                contspec = ContSpec.find_by_product_id_and_name_and_product_type(product.id, "orders", product.product_type) || ContSpec.new(:name => "orders", :product_type => product.product_type, :product_id => product.id)
-                contspec.value = orders
-                contspecs << contspec
+        unless csvfile.blank?
+          File.open(csvfile, 'r') do |f|
+            f.each do |line|
+              /\d+\.,,(?<sku>[^,]+),,(?<rev>"?\$\d+(,\d+)?"?),,,,[^,]+,,(?<orders>\d+)/ =~ line
+              if sku
+                product = Product.find_by_sku(sku)
+                if product
+                  contspec = ContSpec.find_by_product_id_and_name_and_product_type(product.id, "orders", product.product_type) || ContSpec.new(:name => "orders", :product_type => product.product_type, :product_id => product.id)
+                  contspec.value = orders
+                  contspecs << contspec
+                end
               end
             end
           end
