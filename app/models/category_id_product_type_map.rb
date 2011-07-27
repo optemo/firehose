599e45573648d8c7fc5cfaa43d29780b8d5071ef
Ray <ray@rayimac.(none)>
2011-07-26 18:44:37 -0700

    Add validation for feature

--- app/models/category_id_product_type_map.rb
+++ app/models/category_id_product_type_map.rb
@@ -1,3 +1,6 @@
 class CategoryIdProductTypeMap < ActiveRecord::Base
   belongs_to :product_type
+  validates_presence_of :product_type_id
+  validates_presence_of :category_id
+  validates_format_of :category_id, :with => /\d{5}/
 end
--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,5 +1,13 @@
 class Feature < ActiveRecord::Base
   belongs_to :heading, :include => :product_type
+  validates_presence_of :name
+  validates_presence_of :heading_id
+  validates_presence_of :feature_type
+  validates_presence_of :utility_weight
+  validates_presence_of :cluster_weight
+  validates_format_of :utility_weight, :with=>/\d+/
+  validates_format_of :cluster_weight, :with=>/\d+/
+  validates_inclusion_of :feature_type, %w(Continuous Binary Categorical)
   def self.count_products(p_type_name = nil)
     product_type_name = (Session.product_type ||= 'camera_bestbuy') if p_type_name.nil?
     product_type_name ||= p_type_name
