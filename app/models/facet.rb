28599d54d22d8c8cf2a27184b7b75d8e6e508204
Maryam <maryam@maryam.(none)>
2011-09-28 14:46:48 -0700

    a bit of refactoring

--- app/models/facet.rb
+++ app/models/facet.rb
@@ -5,20 +5,16 @@ class Facet < ActiveRecord::Base
   def self.count_products(p_type_name = nil)
      product_type_name = (Session.product_type ||= 'camera_bestbuy') if p_type_name.nil?
      product_type_name ||= p_type_name
-
      facets_to_save = []
      Facet.all.each do |facet|
-       if product_type_name == ProductType.where(["id=?", facet.product_type_id])[0].name 
-         products_counts = 0
-         if facet.feature_type == 'Binary'
+       if facet.feature_type == 'Binary'
+         if product_type_name == ProductType.where(["id=?", facet.product_type_id]).first.name 
            products_counts = BinSpec.count(:conditions =>["name=? and bin_specs.product_type=? and value is not null and value > 0",  facet.name, product_type_name], :joins => "INNER JOIN products on products.id=product_id and products.instock=1")
            facet.active = (products_counts > 0 ? 1 : 0)
            facets_to_save << facet
-
          end
        end
      end
-     print facets_to_save.size
      Facet.import facets_to_save, :on_duplicate_key_update => [:active] if facets_to_save.size > 0
    end
 end
