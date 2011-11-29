b27c29466c70881e20a79ed2b1c7f95b18ea8c1c
Oana Sandu <oana@optemo.com>
2011-11-28 17:25:46 -0800

    Changed utility calculation to include the new customerRating factor

--- app/models/product.rb
+++ app/models/product.rb
@@ -129,6 +129,8 @@ class Product < ActiveRecord::Base
             ori_price = prices[product.id].first.value
             sale_price = records["saleprice"][product.id].first.value
             factorRow.value = Product.calculateFactor_sale(ori_price, sale_price)
+          elsif f.name == "customerRating"
+            factorRow.value = Product.calculateFactor_rating(fVal)
           elsif f.feature_type == "Binary"
             factorRow.value = 1 if fVal
           elsif f.feature_type == "Continuous"
@@ -157,7 +159,6 @@ class Product < ActiveRecord::Base
 
     # Do all record saving at the end for efficiency. :on_duplicate_key_update only works in mysql database
     ContSpec.import cont_activerecords, :on_duplicate_key_update=>[:product_id, :name, :value, :modified]
-
 
     #Clear the search_product cache in the database
     SearchProduct.transaction do
@@ -191,6 +192,11 @@ class Product < ActiveRecord::Base
   
   def self.calculateFactor_sale(fVal1, fVal2) 
      fVal1 > fVal2 ? (fVal1-fVal2)/fVal1 : 0
-  end  
+  end
+  
+  def self.calculateFactor_rating(fVal)
+    fVal >= 4.0 ? 1 : 0
+  end
+  
 end
 class ValidationError < ArgumentError; end
