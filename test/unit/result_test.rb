05e8f9b3a02f64d3174680fb91acc5189a65558a
Monir <Monir@Monirs-MacBook-Air.local>
2011-12-05 16:30:48 -0800

    started to write a test for upkeep_post method

--- test/unit/result_test.rb
+++ test/unit/result_test.rb
@@ -2,7 +2,21 @@ require 'test_helper'
 
 class ResultTest < ActiveSupport::TestCase
   # Replace this with your real tests.
+  
+  setup do
+     Session.new
+   end
+
   test "the truth" do
     assert true
+  end
+  
+  test "upkeep_post" do 
+    p1 = create(:product)
+    p2 = create(:product)
+    p3 = create(:product)
+     create(:bin_spec, name:"featured")
+    create(:bin_spec, name:"featured")
+  
   end
 end
