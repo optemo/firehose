029ccdd52ae3f87d38402e9feb19a39fa527319b
Jan Ulrich <jan@optemo.com>
2011-12-13 14:36:05 -0800

    Using ruby 1.9.3 for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,3 +4,4 @@ require 'bundler/capistrano'
 # The next two lines are needed for integration with Ruby Version Manager:
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.3'
