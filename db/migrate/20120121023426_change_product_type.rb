097156315a3e491c586b70f377e7ab0a8e9c87b1
Jan Ulrich <jan@optemo.com>
2012-02-07 12:05:17 -0800

    Converting product types migration file

--- db/migrate/20120121023426_change_product_type.rb
+++ db/migrate/20120121023426_change_product_type.rb
@@ -1,50 +1,122 @@
 class ChangeProductType < ActiveRecord::Migration
 
     def up
-      add_column :daily_specs, :product_type_id, :integer
-      change_type_in(DailySpec)
-      remove_column :daily_specs, :product_type
-      add_column :bin_specs, :product_type_id, :integer
-      change_type_in(BinSpec)
-      remove_column :bin_specs, :product_type    
-      add_column :cat_specs, :product_type_id, :integer
-      change_type_in(CatSpec)
+      #_specs table
+      remove_column :bin_specs, :product_type
       remove_column :cat_specs, :product_type
-      add_column :cont_specs, :product_type_id, :integer
-      change_type_in(ContSpec)
       remove_column :cont_specs, :product_type
-      add_column :text_specs, :product_type_id, :integer
-      change_type_in(TextSpec)
       remove_column :text_specs, :product_type
+
+      #Products table
+      give_product_new_types
+      remove_column :products, :product_type
+      remove_column :products, :title
+      remove_column :products, :model
+      remove_column :products, :mpn
+      remove_column :products, :created_at
+      remove_column :products, :updated_at
+      
+      #Facets
+      add_column :facets, :product_type, :string
+      convert_type_id(Facet)
+      remove_column :facets, :product_type_id
+
+      #Product Variations
+      remove_column :product_bundles, :product_type
+      remove_column :product_siblings, :product_type
+
+      #Scraping
+      remove_column :scraping_corrections, :product_type
+      convert_type(ScrapingRule)
+      remove_column :scraping_rules, :active
+
+      #Search
+      remove_column :searches, :groupby
+      remove_column :searches, :searchpids
+      remove_column :searches, :searchterm
+      remove_column :searches, :seesim
+      remove_column :searches, :session_id
+      add_column :searches, :product_type, :string
+      
+      #Daily Specs
+      convert_type(DailySpec)
+      
+      #Remove old tables
+      drop_table :candidates
+      drop_table :features
+      drop_table :headings
+      drop_table :results
+      drop_table :urls
+      drop_table :surveys
+      drop_table :search_products
     end
   
     def down
-      add_column :daily_specs, :product_type, :string
-      change_type_back(DailySpec)
-      remove_column :daily_specs, :product_type_id  
+      #_specs table
       add_column :bin_specs, :product_type, :string
-      change_type_back(BinSpec)
-      remove_column :bin_specs, :product_type_id
       add_column :cat_specs, :product_type, :string
-      change_type_back(CatSpec)
-      remove_column :cat_specs, :product_type_id
       add_column :cont_specs, :product_type, :string
-      change_type_back(ContSpec)
-      remove_column :cont_specs, :product_type_id
       add_column :text_specs, :product_type, :string
-      change_type_back(TextSpec)
-      remove_column :text_specs, :product_type_id
+
+      #Products table
+      add_column :products, :product_type, :string
+      add_column :products, :title, :string
+      add_column :products, :model, :string
+      add_column :products, :mpn, :string
+      
+      #Facets
+      add_column :facets, :product_type_id, :integer
+      remove_column :facets, :product_type
+
+      #Product Variations
+      add_column :product_bundles, :product_type, :string
+      add_column :product_siblings, :product_type, :string
+
+      #Scraping
+      add_column :scraping_corrections, :product_type, :string
+      add_column :scraping_rules, :active, :boolean
+
+      #Search
+      add_column :searches, :groupby, :string
+      add_column :searches, :searchpids, :string
+      add_column :searches, :searchterm, :string
+      add_column :searches, :seesim, :string
+      add_column :searches, :session_id, :integer
+      remove_column :searches, :product_type
     end
   
-  def change_type_in(m)
-    m.where("product_type = 'camera_bestbuy'").update_all(:product_type_id => 2)
-    m.where("product_type = 'tv_bestbuy'").update_all(:product_type_id => 4)
-    m.where("product_type = 'drive_bestbuy'").update_all(:product_type_id => 6)
+  def give_product_new_types
+    Product.all.each do |p|
+      new_type = type_to_str(p.product_type)
+      CatSpec.create name: 'product_type', value: new_type, product_id: p.id
+    end
   end
   
-  def change_type_back(m)
-    m.where("product_type_id = 2").update_all(:product_type => 'camera_bestbuy')
-    m.where("product_type_id = 4").update_all(:product_type => 'tv_bestbuy')
-    m.where("product_type_id = 6").update_all(:product_type => 'drive_bestbuy')
+  def convert_type_id(model)
+    model.all.each do |p|
+      p.update_attribute(:product_type, type_id_to_str(p.product_type_id)
+    end
+  end
+  
+  def convert_type(model)
+    model.all.each do |p|
+      p.update_attribute(:product_type, type_to_str(p.product_type)
+    end
+  end
+  
+  def type_to_str(type)
+    case type
+      when 'camera_bestbuy' then 'B20218'
+      when 'drive_bestbuy' then 'B20232'
+      when 'tv_bestbuy' then
+    end
+  end
+  
+  def type_id_to_str(type_id)
+    case type_id
+      when 2 then 'B20218'
+      when 4 then
+      when 6 then 'B20232'
+    end
   end
 end
