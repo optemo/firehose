bd75c94917eb8beb720298d572a82c6c508c2de9
Oana Sandu <oana@optemo.com>
2012-02-07 10:31:12 -0800

    changes to specs tables

--- db/migrate/20120121023426_change_product_type.rb
+++ db/migrate/20120121023426_change_product_type.rb
@@ -1,23 +1,50 @@
 class ChangeProductType < ActiveRecord::Migration
-  def up
-    # add column product_type_id
-    add_column :bin_specs, :product_type_id, :integer
-    BinSpec.where("product_type = 'camera_bestbuy'").update_all(:product_type_id => 2)
-    BinSpec.where("product_type = 'tv_bestbuy'").update_all(:product_type_id => 4)
-    BinSpec.where("product_type = 'drive_bestbuy'").update_all(:product_type_id => 6)
-    # for each of product_type, fill with corresponding product_type_id
-    # delete column product_type
-    remove_column :bin_specs, :product_type
-  end
 
-  def down
-    add_column  :bin_specs, :product_type, :string
-    BinSpec.where("product_type_id = 2").update_all(:product_type => 'camera_bestbuy')
-    BinSpec.where("product_type_id = 4").update_all(:product_type => 'tv_bestbuy')
-    BinSpec.where("product_type_id = 6").update_all(:product_type => 'drive_bestbuy')
-    remove_column :bin_specs, :product_type_id
-    # add column product_type
-    # for each of product_type_id, fill with corresponding product_type
-    # delete column product_type_id
+    def up
+      add_column :daily_specs, :product_type_id, :integer
+      change_type_in(DailySpec)
+      remove_column :daily_specs, :product_type
+      add_column :bin_specs, :product_type_id, :integer
+      change_type_in(BinSpec)
+      remove_column :bin_specs, :product_type    
+      add_column :cat_specs, :product_type_id, :integer
+      change_type_in(CatSpec)
+      remove_column :cat_specs, :product_type
+      add_column :cont_specs, :product_type_id, :integer
+      change_type_in(ContSpec)
+      remove_column :cont_specs, :product_type
+      add_column :text_specs, :product_type_id, :integer
+      change_type_in(TextSpec)
+      remove_column :text_specs, :product_type
+    end
+  
+    def down
+      add_column :daily_specs, :product_type, :string
+      change_type_in(DailySpec)
+      remove_column :daily_specs, :product_type_id  
+      add_column :bin_specs, :product_type, :string
+      change_type_in(BinSpec)
+      remove_column :bin_specs, :product_type_id
+      add_column :cat_specs, :product_type, :string
+      change_type_in(CatSpec)
+      remove_column :cat_specs, :product_type_id
+      add_column :cont_specs, :product_type, :string
+      change_type_in(ContSpec)
+      remove_column :cont_specs, :product_type_id
+      add_column :text_specs, :product_type, :string
+      change_type_in(TextSpec)
+      remove_column :text_specs, :product_type_id
+    end
+  
+  def change_type_in(m)
+    m.where("product_type = 'camera_bestbuy'").update_all(:product_type_id => 2)
+    m.where("product_type = 'tv_bestbuy'").update_all(:product_type_id => 4)
+    m.where("product_type = 'drive_bestbuy'").update_all(:product_type_id => 6)
+  end
+  
+  def change_type_back(m)
+    m.where("product_type_id = 2").update_all(:product_type => 'camera_bestbuy')
+    m.where("product_type_id = 4").update_all(:product_type => 'tv_bestbuy')
+    m.where("product_type_id = 6").update_all(:product_type => 'drive_bestbuy')
   end
 end
