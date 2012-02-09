415ab00cbad7824a4f01303f9221e137b5bbc4ac
Monir <Monir@Monirs-MacBook-Air.local>
2012-02-09 15:51:26 -0800

    added some more test cases to product_category_test

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,44 +1,64 @@
 class ProductCategory < ActiveRecord::Base
   
+  
   def self.get_children(node, retailer, level=nil)
     root = ProductCategory.where(:product_type => node).first
-    result = ProductCategory.where("l_id > ? and r_id < ? and retailer = ?", root.l_id, root.r_id, retailer)
-    unless level.nil?
-      result = result.where(:level => level)
+    if root
+      result = ProductCategory.where("l_id > ? and r_id < ? and retailer = ?", root.l_id, root.r_id, retailer)
+      unless level.nil?
+        result = result.where(:level => level)
+      end
+      return result
     end
-    return result
   end
   
   def self.get_ancestors(node, retailer, level=nil)
     root = ProductCategory.where(:product_type => node).first
-    result = ProductCategory.where("l_id < ? and r_id > ? and retailer = ?", root.l_id, root.r_id, retailer)
-    unless level.nil?
-      result = result.where(:level => level)
+    if root
+      result = ProductCategory.where("l_id < ? and r_id > ? and retailer = ?", root.l_id, root.r_id, retailer)
+      unless level.nil?
+        result = result.where(:level => level)
+      end
+     #result.each do |r|
+     #  puts "#{r.product_type} #{r.l_id} #{r.r_id} #{r.level}"
+     #end
+      return result
     end
-    return result
   end
   
   def self.get_subcategories(node, retailer)
     root = ProductCategory.where(:product_type => node).first
-    result = ProductCategory.where("l_id > ? and r_id < ? and retailer = ?", root.l_id, root.r_id, retailer).where("level = ?", root.level+1)
-    return result
+    if root
+      result = ProductCategory.where("l_id > ? and r_id < ? and retailer = ?", root.l_id, root.r_id, retailer).where("level = ?", root.level+1)
+     #result.each do |r|
+     #  puts "#{r.product_type} #{r.l_id} #{r.r_id} #{r.level}"
+     #end
+      return result
+    end
   end  
   
   def self.get_parent(node, retailer)
     root = ProductCategory.where(:product_type => node).first
-    result = ProductCategory.where("l_id < ? and r_id > ? and retailer = ?", root.l_id, root.r_id, retailer).where("level = ?", root.level-1)
-    return result
+    if root
+      result = ProductCategory.where("l_id < ? and r_id > ? and retailer = ?", root.l_id, root.r_id, retailer).where("level = ?", root.level-1)
+      return result.first
+    end
   end
   
-  def leaves (node)
+  def self.leaves (node)
     node = [node] unless node.class == Array
     node = node[0..0] if Rails.env.test? #Only check first node for testing
     leaves=[]
+    retailer = get_retailer(node[0])
     node.each do |n|
-      leaves = children(n).where("l_id == (r_id-1)").map{|r| r.product_type}
+      leaves += get_children(n, retailer).where("l_id = (r_id-1)").map{|r| r.product_type}.uniq
     end
-    leaves.each do |l|
-      puts "l"
-    end
+    #leaves.each do |l|
+     # puts "#{l}"
+    #end
+    leaves
+  end
+  def self.get_retailer(node)
+     node[0,1]== 'B' ? "bestbuy" : "futureshop"
   end
 end
--- test/unit/product_category_test.rb
+++ test/unit/product_category_test.rb
@@ -1,9 +1,29 @@
-require 'test_helper'
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../../config/environment', __FILE__)
+require 'rails/test_help'
 
 class ProductCategoryTest < ActiveSupport::TestCase
  
-  test "check leaf nodes" do
-    leaves = ProductCategory.leaves("B20213")
-   #assert true
+  test "test get_children and leaves methods" do  
+    assert_equal [], ProductCategory.leaves("B28597")
+    assert_equal 3, ProductCategory.leaves("B30317a").count
   end
+  
+  test "test get_ancestors method" do  
+    assert_equal [], ProductCategory.get_ancestors("BDepartments", "bestbuy")
+    assert_nil ProductCategory.get_ancestors("BDepart", "bestbuy")
+    assert_equal "B20001", ProductCategory.get_ancestors("B21202", "bestbuy", 2).first.product_type
+    assert ProductCategory.get_ancestors("B21202", "bestbuy")
+  end
+  
+  test "test get_parents method" do  
+     assert_equal "B30317a", ProductCategory.get_parent("B29182", "bestbuy").product_type
+     assert_nil ProductCategory.get_parent("23456", "futureshop")
+  end
+  
+   test "test get_subcategories method" do  
+       assert ProductCategory.get_subcategories("B20001", "bestbuy")
+       assert_equal [],ProductCategory.get_subcategories("B30938", "bestbuy")
+   end
+  
 end
