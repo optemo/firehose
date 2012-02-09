5c219c66ed5cde6f491d53ca7ee10022fdc24317
Oana Sandu <oana@optemo.com>
2012-02-09 11:49:19 -0800

    added function for the children (descendants) of a Product Category

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,2 +1,12 @@
 class ProductCategory < ActiveRecord::Base
+  
+  def self.children(node, level, retailer)
+    root = ProductCategory.where(:product_type => node, :retailer => retailer).first
+    children = ProductCategory.where("l_id > ? and r_id < ? and retailer = ?", root.l_id, root.r_id, retailer)
+    unless level.nil?
+      children = children.where(:level => level)
+    end
+    return children
+  end
+  
 end
