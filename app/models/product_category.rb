c7e860620087435d8be06c0011ff16063e90b40c
Oana Sandu <oana@optemo.com>
2012-02-09 15:42:03 -0800

    functions for product categories now supporting several input ids

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,33 +1,38 @@
 class ProductCategory < ActiveRecord::Base
   
-  def self.get_children(node, retailer, level=nil)
-    root = ProductCategory.where(:product_type => node, :retailer => retailer).first
-    result = ProductCategory.where("l_id > ? and r_id < ? and retailer = ?", root.l_id, root.r_id, retailer)
-    unless level.nil?
-      result = result.where(:level => level)
-    end
-    return result
+  def self.get_children(nodes, level=nil)
+    nodes = [nodes] unless nodes.class == Array
+    search = build_query(nodes, left="l_id > ", right="r_id < ", level)
+    return ProductCategory.where(search)
   end
   
-  def self.get_ancestors(node, retailer, level=nil)
-    root = ProductCategory.where(:product_type => node, :retailer => retailer).first
-    result = ProductCategory.where("l_id < ? and r_id > ? and retailer = ?", root.l_id, root.r_id, retailer)
-    unless level.nil?
-      result = result.where(:level => level)
-    end
-    return result
+  def self.get_ancestors(nodes, level=nil)
+    nodes = [nodes] unless nodes.class == Array
+    search = build_query(nodes, left="l_id < ", right="r_id > ", level)
+    return ProductCategory.where(search)
   end
   
-  def self.get_subcategories(node, retailer)
-    root = ProductCategory.where(:product_type => node, :retailer => retailer).first
-    result = ProductCategory.where("l_id > ? and r_id < ? and retailer = ?", root.l_id, root.r_id, retailer).where("level = ?", root.level+1)
-    return result
+  def self.get_subcategories(node)
+    root = ProductCategory.where(:product_type => node).first
+    return ProductCategory.where("l_id > ? and r_id < ? and retailer = ? and level = ?", root.l_id, root.r_id, root.retailer, root.level+1)
   end  
   
-  def self.get_parent(node, retailer)
-    root = ProductCategory.where(:product_type => node, :retailer => retailer).first
-    result = ProductCategory.where("l_id < ? and r_id > ? and retailer = ?", root.l_id, root.r_id, retailer).where("level = ?", root.level-1)
-    return result
+  def self.get_parent(node)
+    root = ProductCategory.where(:product_type => node).first
+    return ProductCategory.where("l_id < ? and r_id > ? and retailer = ? and level = ?", root.l_id, root.r_id, root.retailer, root.level-1) 
+  end
+  
+  def self.build_query(nodes, left, right, level)
+    overall_search = []
+    nodes.each do |node|
+      root = ProductCategory.where(:product_type => node).first
+      search = ""
+      search << "(#{left}#{root.l_id} and #{right}#{root.r_id} and retailer = '#{root.retailer}'"
+      search << " and level = #{level}" unless level.nil?
+      search << ")"
+      overall_search << search
+    end
+    return overall_search.join(" OR ")
   end
   
 end
