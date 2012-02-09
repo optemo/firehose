a3d0a6d4c9104cb90106d653350dd6eb062c3b22
Oana Sandu <oana@optemo.com>
2012-02-09 12:09:37 -0800

    implemented other functions for traversing the product category hierarchy

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,12 +1,33 @@
 class ProductCategory < ActiveRecord::Base
   
-  def self.children(node, level, retailer)
+  def self.get_children(node, retailer, level=nil)
     root = ProductCategory.where(:product_type => node, :retailer => retailer).first
-    children = ProductCategory.where("l_id > ? and r_id < ? and retailer = ?", root.l_id, root.r_id, retailer)
+    result = ProductCategory.where("l_id > ? and r_id < ? and retailer = ?", root.l_id, root.r_id, retailer)
     unless level.nil?
-      children = children.where(:level => level)
+      result = result.where(:level => level)
     end
-    return children
+    return result
+  end
+  
+  def self.get_ancestors(node, retailer, level=nil)
+    root = ProductCategory.where(:product_type => node, :retailer => retailer).first
+    result = ProductCategory.where("l_id < ? and r_id > ? and retailer = ?", root.l_id, root.r_id, retailer)
+    unless level.nil?
+      result = result.where(:level => level)
+    end
+    return result
+  end
+  
+  def self.get_subcategories(node, retailer)
+    root = ProductCategory.where(:product_type => node, :retailer => retailer).first
+    result = ProductCategory.where("l_id > ? and r_id < ? and retailer = ?", root.l_id, root.r_id, retailer).where("level = ?", root.level+1)
+    return result
+  end  
+  
+  def self.get_parent(node, retailer)
+    root = ProductCategory.where(:product_type => node, :retailer => retailer).first
+    result = ProductCategory.where("l_id < ? and r_id > ? and retailer = ?", root.l_id, root.r_id, retailer).where("level = ?", root.level-1)
+    return result
   end
   
 end
