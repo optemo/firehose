4ca0478992568b1a0753e037ac33535a3cb7c23e
Oana Sandu <oana@optemo.com>
2012-02-09 16:57:01 -0800

    added memcache to product category

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -3,23 +3,33 @@ class ProductCategory < ActiveRecord::Base
   def self.get_children(nodes, level=nil)
     nodes = [nodes] unless nodes.class == Array
     search = build_query(nodes, left="l_id > ", right="r_id < ", level)
-    return ProductCategory.where(search).map{|x| x.product_type}
+    CachingMemcached.cache_lookup("ProductCategory#{search.hash}") do
+      ProductCategory.where(search).map(&:product_type)
+    end
   end
   
   def self.get_ancestors(nodes, level=nil)
     nodes = [nodes] unless nodes.class == Array
     search = build_query(nodes, left="l_id < ", right="r_id > ", level)
-    return ProductCategory.where(search).map{|x| x.product_type}
+    CachingMemcached.cache_lookup("ProductCategory#{search.hash}") do
+      ProductCategory.where(search).map(&:product_type)
+    end
   end
   
   def self.get_subcategories(node)
     root = ProductCategory.where(:product_type => node).first
-    return ProductCategory.where("l_id > ? and r_id < ? and retailer = ? and level = ?", root.l_id, root.r_id, root.retailer, root.level+1).map{|x| x.product_type}
+    search = "l_id > #{root.l_id} and r_id < #{root.r_id} and retailer = '#{root.retailer}' and level = #{root.level+1}"
+    CachingMemcached.cache_lookup("ProductCategory#{search.hash}") do
+        ProductCategory.where(search).map(&:product_type)
+    end
   end  
   
   def self.get_parent(node)
     root = ProductCategory.where(:product_type => node).first
-    return ProductCategory.where("l_id < ? and r_id > ? and retailer = ? and level = ?", root.l_id, root.r_id, root.retailer, root.level-1) .map{|x| x.product_type}
+    search = "l_id < #{root.l_id} and r_id > #{root.r_id} and retailer = '#{root.retailer}' and level = #{root.level-1}"
+    CachingMemcached.cache_lookup("ProductCategory#{search.hash}") do
+      ProductCategory.where(search).map(&:product_type)
+    end
   end
   
   def self.build_query(nodes, left, right, level)
