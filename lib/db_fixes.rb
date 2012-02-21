6a4bb912f4e3529049082f291c86412dc7610629
Marc Lejay <marc.lejay@hotmail.com>
2012-02-21 09:33:54 -0800

    Finished task for recommended products/accessories

--- lib/db_fixes.rb
+++ lib/db_fixes.rb
@@ -10,21 +10,21 @@
 #    new_id += 2
 #  end
 #end
+def set_missing_ids ()
+ #finds largest id number, sets the new id to this+2
+ new_id = DailySpec.select(:id).max.id + 2
+ #only alters rows with id = 0
+ 
+ for i in DailySpec.find_all_by_id(0)
+   debugger  
+   ds = i
+   ds = DailySpec.new(i.attributes)
+   ds.id = new_id    #id is not being set! (works if only 1 zero row, not if multiple)
+   ds.save
+   new_id += 2
+ end
+end
 
 #def set_missing_ids ()
-# #finds largest id number, sets the new id to this+2
-# new_id = DailySpec.select(:id).max.id + 2
-# #only alters rows with id = 0
-# for i in DailySpec.find_all_by_id(0)
-#   debugger  
-#   ds = i
-#   ds = DailySpec.new(i.attributes)
-#   ds.id = new_id    #id is not being set! (works if only 1 zero row, not if multiple)
-#   ds.save
-#   new_id += 2
-# end
+# #possible fix at:  http://railsforum.com/viewtopic.php?id=14250  ?
 #end
-
-def set_missing_ids ()
- #possible fix at:  http://railsforum.com/viewtopic.php?id=14250  ?
-end
