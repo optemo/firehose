3a53be2565196c4c14b4ff68ebb544a48b290ed1
Oana Sandu <oana@optemo.com>
2012-02-29 16:39:51 -0800

    work in progress on customizations

--- app/models/customization.rb
+++ app/models/customization.rb
@@ -25,10 +25,21 @@ class Customization
     # then according to the rule_type, go to one of the _specs tables and look up the feature value
     # then add the feature value to a hash by the original feature [name]
     local_features = []
-    features.each do |remote_feature|
-      sr = ScrapingRule.find_by_remote_featurename_and_product_type(remote_feature, Session.product_type_path)
-      spec_class = Customization.rule_type_to_class(sr.rule_type)
-      local_features += [{spec_class => sr.local_featurename}]
+    features.each do |local_feature|
+      debugger
+      sr = ScrapingRule.find_by_local_featurename_and_product_type(local_feature, Session.product_type_path)
+      
+      if sr.nil?
+        # This is only for DailySpecs orders
+        if local_feature == 'orders'
+          spec_class = DailySpec
+        else
+          raise 'No scraping rule found matching feature ' + local_feature
+        end
+      else
+        spec_class = Customization.rule_type_to_class(sr.rule_type)
+      end
+      local_features += [{spec_class => local_feature}]
     end
     local_features
   end
@@ -48,80 +59,164 @@ class Customization
     results = {}
     # execute each of the rules
     rules.each do |rule|
-      input_features = Customization.get_needed_features(rule.needed_features, product_type)
-      rule_results = rule.compute(skus, input_features)
-      spec_class = rule_results[0].class
-      debugger
-      results.has_key?(spec_class) ? results[spec_class] += rule_results : results[spec_class] = rule_results
+      spec_features = Customization.get_needed_features(rule.needed_features, product_type)
+      #rule_results = rule.compute(skus, spec_features)
+      # RuleNew.compute(skus, spec_features)
+      rule_results = []
+      # assumption: there are scraping rules for the input features, and these have been scraped already
+      # if an sku doesn't have a required spec value in the table, passing nil value to feature computation
+      skus.each do |sku|
+        # find the product id for this sku
+        prod = Product.find_by_sku(sku)
+        if prod.nil?
+          # sku not found
+          raise 'SKU ' + sku + ' not found in Products'
+        end
+        pid = prod.id
+        values = []
+        spec_features.each do |spec_feature|
+          table_name = spec_feature.keys[0]
+          feature_name = spec_feature.values[0]
+          # This is only for DailySpecs orders
+          if table_name == DailySpec
+            spec_row = table_name.find_by_sku_and_name(sku, feature_name)
+          else
+            spec_row = table_name.find_by_product_id_and_name(pid, feature_name)
+          end
+          if spec_row.nil?
+            values += [nil]
+          else
+            values += [spec_row.value]
+          end
+        end
+        # actual computation logic
+        spec = rule.compute_feature(values, pid)
+        rule_results += [spec] unless spec.nil?
+      end
+      
+      unless rule_results.empty?
+        spec_class = rule_results[0].class
+        results.has_key?(spec_class) ? results[spec_class] += rule_results : results[spec_class] = rule_results
+      end
     end
+    # debugger
     results
   end
 end
 
-# class RulePreorder < Customization
-#   @feature_name = 'preorder'
-#   @needed_features = ['PreorderReleaseDate']
-#   @product_type = 'F1127'
-#   @rule_type = 'Binary'
-#   
-#   # This subclass inherits all the class methods of the parent
-#   # Customization.all and self.all are equivalent
-#   
-#   def RulePreorder.compute(skus, input_features)
-#     #Customization.product_type = @product_type
-#     return []
-#   end
-# end
-
-class RuleNew < Customization
-  @feature_name = 'is_new'
+class RuleComingSoon < Customization
+  @feature_name = 'comingSoon'
+  @needed_features = ['preorderDate']
   @product_type = 'F1127'
-  @needed_features = ['DisplayStartDate', 'PreorderReleaseDate']
   @rule_type = 'Binary'
   
-  
+  def RuleComingSoon.compute_feature(values, pid)
+    preoder_val = values[0]
+    return nil if preoder_val == nil
+    derived_value = (Date.parse(preoder_val) - Date.today > 0)
+    #derived_value = values.inject(false) { |result,val| result or val.nil? ? false : (Date.today - Date.parse(val) <= 30) }
+    spec_class = Customization.rule_type_to_class(@rule_type)
+    # if the value is false, we don't want to return (and store) a spec, we want to delete it, so do it here
+    spec = nil
+    if derived_value == false
+      spec = spec_class.find_by_product_id_and_name(pid, @feature_name)
+      spec_class.delete(spec) unless spec.nil?
+    else
+      spec = spec_class.find_or_initialize_by_product_id_and_name(pid, @feature_name)
+      spec.value = derived_value
+    end
+    spec
+  end
+end
+
+class RuleNew < Customization
+  @feature_name = 'isNew'
+  @product_type = 'F1127'
+  @needed_features = ['displayDate', 'preorderDate']
+  @rule_type = 'Binary'
   # compute functionality:
-  # for the needed_features, get their values from the DB :)
+  # for the needed_features, get their values from the DB
   # then execute the function's own computation for calculating the value
   # then *save* the value in the table represented by @rule_type, under name @feature_name
-  def RuleNew.compute(skus, spec_features)
-    specs_to_save = []
-    print "Computing value for RuleNew and skus"
-    print skus
-    # assumption: there are scraping rules for the input features, and these have been scraped already
-    # if an sku doesn't have a required spec value in the table, passing nil value to feature computation
-    skus.each do |sku|
-      # find the product id for this sku
-      prod = Product.find_by_sku(sku)
-      if prod.nil?
-        # sku not found
-        raise 'SKU ' + sku + ' not found in Products'
-      end
-      pid = prod.id
-      values = []
-      spec_features.each do |spec_feature|
-        table_name = spec_feature.keys[0]
-        feature_name = spec_feature.values[0]
-        spec_row = table_name.find_by_product_id_and_name(pid, feature_name)
-        if spec_row.nil?
-          values += [nil]
-        else
-          values += [spec_row.value]
-        end
-      end
-      # actual computation logic
-      specs_to_save += [RuleNew.compute_feature(values)]
-    end
-    specs_to_save
-  end
+  # def RuleNew.compute(skus, spec_features)
+  # 
+  #   
+  #   specs_to_save
+  # end
 
-  def RuleNew.compute_feature(values)
+  def RuleNew.compute_feature(values, pid)
     # assumption: the values are in the same order as the needed_features, but this doesn't matter for this rule
     # if either of the values (dates) are within 30 days of today, make a spec with a true value
     derived_value = values.inject(false) { |result,val| result or val.nil? ? false : (Date.today - Date.parse(val) <= 30) }
     spec_class = Customization.rule_type_to_class(@rule_type)
-    spec = spec_class.find_or_initialize_by_product_id_and_name(pid, @feature_name)
-    spec.value = derived_value
+    # if the value is false, we don't want to return (and store) a spec, we want to delete it, so do it here
+    spec = nil
+    if derived_value == false
+      spec = spec_class.find_by_product_id_and_name(pid, @feature_name)
+      spec_class.delete(spec) unless spec.nil?
+    else
+      spec = spec_class.find_or_initialize_by_product_id_and_name(pid, @feature_name)
+      spec.value = derived_value
+    end
     spec
   end
 end
+
+class RuleOnsale < Customization
+  @feature_name = 'newonsale'
+  @product_type = 'F1127'
+  @needed_features = ['saleEndDate']
+  @rule_type = 'Binary'
+
+  def RuleOnsale.compute_feature(values, pid)
+    # if either of the values (dates) are within 30 days of today, make a spec with a true value
+    val = values[0]
+    return nil if val == nil
+    derived_value = (Time.parse(val) - 4.hours) > Time.now
+    spec_class = Customization.rule_type_to_class(@rule_type)
+    # if the value is false, we don't want to return (and store) a spec, we want to delete it, so do it here
+    spec = nil
+    if derived_value == false
+      spec = spec_class.find_by_product_id_and_name(pid, @feature_name)
+      spec_class.delete(spec) unless spec.nil?
+    else
+      spec = spec_class.find_or_initialize_by_product_id_and_name(pid, @feature_name)
+      spec.value = derived_value
+    end
+    spec
+  end
+end
+
+
+# class RuleBestseller < Customization
+#   @feature_name = 'bestseller'
+#   @product_type = 'F1127'
+#   @needed_features = ['orders']
+#   @rule_type = 'Binary'
+#   # compute functionality:
+#   # for the needed_features, get their values from the DB
+#   # then execute the function's own computation for calculating the value
+#   # then *save* the value in the table represented by @rule_type, under name @feature_name
+#   # def RuleNew.compute(skus, spec_features)
+#   # 
+#   #   
+#   #   specs_to_save
+#   # end
+# 
+#   def RuleNew.compute_feature(values, pid)
+#     # assumption: the values are in the same order as the needed_features, but this doesn't matter for this rule
+#     # if either of the values (dates) are within 30 days of today, make a spec with a true value
+#     derived_value = (values[0] > 1)
+#     spec_class = Customization.rule_type_to_class(@rule_type)
+#     # if the value is false, we don't want to return (and store) a spec, we want to delete it, so do it here
+#     spec = nil
+#     if derived_value == false
+#       spec = spec_class.find_by_product_id_and_name(pid, @feature_name)
+#       spec_class.delete(spec) unless spec.nil?
+#     else
+#       spec = spec_class.find_or_initialize_by_product_id_and_name(pid, @feature_name)
+#       spec.value = derived_value
+#     end
+#     spec
+#   end
+# end
