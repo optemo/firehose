36562cf366e3339993d3e16bebf72addbe7d8869
Marc Lejay <marc.lejay@hotmail.com>
2012-04-03 10:43:31 -0700

    Product type in rules page on firehose + deletion of bestseller_store_sales before rewriting it for a given category

--- app/helpers/scraping_rules_helper.rb
+++ app/helpers/scraping_rules_helper.rb
@@ -1,2 +1,11 @@
 module ScrapingRulesHelper
+  # More thorough than getting the type of only one rule (though there should not be more than one type)
+  def get_feature_type (rules)
+    rule_types = rules.map{|rule| rule.rule_type}.uniq
+    feature_type = ""
+    rule_types.each do |rule_type|
+      feature_type << rule_type << " "
+    end
+    feature_type
+  end
 end
