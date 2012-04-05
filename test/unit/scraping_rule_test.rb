82b7f7cd20d2b016f401d8e1cb352345eca347a8
Marc Lejay <marc.lejay@hotmail.com>
2012-04-05 09:00:39 -0700

    Added tests for changed regex separator

--- test/unit/scraping_rule_test.rb
+++ test/unit/scraping_rule_test.rb
@@ -21,7 +21,27 @@ class ScrapingRuleTest < ActiveSupport::TestCase
   end
   
   test "scraping" do
-    ScrapingRule.scrape(BBproduct.new id: "100000", category: "B20218")
-    assert true
+    Session.new("B20218")
+    # Make Rules (these are applied according to when they were defined -> first one will give first candidate)
+    create :scraping_rule, local_featurename: 'regular', remote_featurename: 'specs.Physical Features.Colour', rule_type: 'Categorical', regex: 'Black|Blue|Brown' 
+    create :scraping_rule, local_featurename: 'consecutive_fr_trans', remote_featurename: 'specs.Physical Features.Colour', rule_type: 'Categorical', regex: 'Pink/Rose^^Black/Noir^^Bla/Noi'
+    create :scraping_rule, local_featurename: 'consecutive_order', remote_featurename: 'specs.Physical Features.Colour', rule_type: 'Categorical', regex: 'Pink/Rose^^Bla/Noi^^Black/Noir'
+    
+    # Call scraping on products
+    candidates1 = ScrapingRule.scrape((BBproduct.new id: "100000", category: "B20218"),false,[],false) # Colour: Black
+    candidates2 = ScrapingRule.scrape((BBproduct.new id: "100001", category: "B20218"),false,[],false) # Colour: Silver
+    
+    # Check candidates
+      # regular regex
+      assert_equal candidates1[0].parsed, "Black", "Data should have matched"
+      assert_equal candidates2[0].parsed, nil,  "Data should not have matched"
+      
+      # consecutive_fr_trans regex
+      assert_equal candidates1[1].parsed, "Noir", "Data should have matched"
+      assert_equal candidates2[1].parsed, nil,  "Data should not have matched"
+        
+      # consecutive_order
+      assert_equal candidates1[2].parsed, "Noi", "Should have returned the first match"
+      assert_equal candidates2[2].parsed, nil,  "Data should not have matched"
   end
 end
