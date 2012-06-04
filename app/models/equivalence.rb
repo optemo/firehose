e879d642967a54b94ee1428046a5f38ad30c4ff1
Jan Ulrich <jan@optemo.com>
2012-06-04 13:29:21 -0700

    Added bundles to the equivalence class

--- app/models/equivalence.rb
+++ app/models/equivalence.rb
@@ -6,9 +6,10 @@ class Equivalence < ActiveRecord::Base
       #Siblings is a symmetric and transitive relationship
       #while product bundles is non-symmetric
       eq = Equivalence.find_or_initialize_by_product_id(prod.id)
-      #Take out product bundles from Equivalences
-      #equivalences = prod.product_bundles.map(&:bundle_id)+ProductBundle.find_all_by_bundle_id(prod.id).map(&:product_id)+prod.product_siblings.map(&:sibling_id)
-      equivalences = prod.product_siblings.map(&:sibling_id)
+      #Put product bundles back into Equivalences
+      equivalences = prod.product_bundles.map(&:bundle_id)+ProductBundle.find_all_by_bundle_id(prod.id).map(&:product_id)+prod.product_siblings.map(&:sibling_id)
+      #This is for taking bundles back out
+      #equivalences = prod.product_siblings.map(&:sibling_id)
 
       if sibling = eq_ar.index{|p| equivalences.include? p.product_id}
         eq.eq_id = eq_ar[sibling].eq_id
