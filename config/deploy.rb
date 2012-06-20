e77a6861564d666fd073a2b872529515804e53d6
Jan Ulrich <jan@optemo.com>
2012-06-19 18:49:06 -0700

    Got deployment to jaguar working again

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
 set :stages, %w(jaguar linode slicehost uniserve-firehose)
+# The next two lines are needed for integration with Ruby Version Manager:
+set :rvm_type, :system
+set :rvm_ruby_string, '1.9.3'
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
-# The next two lines are needed for integration with Ruby Version Manager:
-set :rvm_ruby_string, '1.9.3'
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
