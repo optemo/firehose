8e63078abfa562162b2f89f3a8271c4f7c8611a4
Nick Sawadsky <nick@optemo.com>
2012-08-01 15:03:46 -0700

    When Best Buy API returns all products in catalog for a particular category, assume category is empty (rather than raising an error).

--- app/models/best_buy_api.rb
+++ app/models/best_buy_api.rb
@@ -159,7 +159,8 @@ class BestBuyApi
         feed_category = BestBuyApi.get_category(my_id)
         root_category = BestBuyApi.get_category('Departments')
         if (feed_category['productCount'] == root_category['productCount'] and my_id != 'Departments')
-          raise BestBuyApi::RequestError, ('Invalid category ' + id.to_s)
+          puts "Category " + my_id + " appears to be empty"
+          return ids
         end
         page = 1
         totalpages = nil
