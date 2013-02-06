e83725cd9f4b514c11ca2813669703f1c5177a6d
Flynn Howling <flynn@optemo.com>
2013-02-06 09:55:06 -0800

    Updating accessories page to use downloaded inmages in jaguar
    Change-Id: Ib758555eb03dea59370f6dcf4dc59a1f0b5877e3

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -4,14 +4,14 @@ class Accessory < ActiveRecord::Base
   def img_url
     retailer = CatSpec.find_by_name_and_product_id("product_type",value).try(:value)
     if retailer =~ /^B/
-      url = "http://www.bestbuy.ca/multimedia/Products/150x150/"
+      url = "http://firehose-demo/assets/product_images/bb_images/"
     elsif retailer =~ /^F/
-      url = "http://www.futureshop.ca/multimedia/Products/250x250/"
+      url = "http://firehose-demo/assets/product_images/futureshop_images/"
     else
       raise "No known image link for product: #{sku}"
     end
     
-    url += sku[0..2].to_s+"/"+sku[0..4].to_s+"/"+sku.to_s+".jpg"
+    url += sku + ".jpg"
   end
   
   def sku
